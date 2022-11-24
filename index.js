4e414462dc7454f5ee4929dc086ba228e3b10949
Lucas González <lucasalb0120@gmail.com>
2022-11-24 19:13:16 -0300

    nuevo input field, + saving data in localstorage

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const pass_field = document.querySelector('.pass-key')
 const showBtn = document.querySelector('.show')
+const username = document.getElementById('username')
+const register = document.getElementById('register')
+
 showBtn.addEventListener('click', function () {
   if (pass_field.type === 'password') {
     pass_field.type = 'text'
@@ -10,4 +13,14 @@ showBtn.addEventListener('click', function () {
     showBtn.textContent = 'SHOW'
     showBtn.style.color = '#222'
   }
+})
+
+let valor = ''
+
+username.addEventListener('input', (e) => {
+  valor = e.target.value
+})
+
+register.addEventListener('click', (e) => {
+  localStorage.setItem('username', valor)
 })
